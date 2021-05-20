275200933bde9999e606a24595da9d95f9c3f5ae
JawalArcilla <>
2021-05-20 02:03:06 +0000

    Fixed errors

--- script.js
+++ script.js
@@ -9,9 +9,9 @@ function check () {
   day = document.getElementById('day').value
   day = parseInt(day)
 
-  if ((day === 'Monday' || day === 'Tuesday' || day === 'Wednesday' || day === 'Thursday' || day === 'Friday' | (userInput >5 & userInput <18))) {
+  if ((day === 'Monday' || day === 'Tuesday' || day === 'Wednesday' || day === 'Thursday' || day === 'Friday' | (userInput > 5 & userInput < 18))) {
     document.getElementById('answer').innerHTML = 'Time to go to school'
-  } else if (day === 'Monday' || day === 'Tuesday' || day === 'Wednesday' || day === 'Thursday' || day === 'Friday' | (userInput >18)) {
+  } else if (day === 'Monday' || day === 'Tuesday' || day === 'Wednesday' || day === 'Thursday' || day === 'Friday' | (userInput > 18)) {
     document.getElementById('answer').innerHTML = 'Time to go to work'
   } else {
     document.getElementById('answer').innerHTML = 'Time to relax for the weekend'
